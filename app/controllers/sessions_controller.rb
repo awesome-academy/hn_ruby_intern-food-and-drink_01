4b45858d5cce8a6ee75c87646559e1933af73997
MinhNamThan <thanminhnam17022001@gmail.com>
2022-08-18 08:28:41 +0700

    Add login logout and admin routes

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by email: params[:session][:email].downcase
+    if user&.authenticate(params[:session][:password])
+      log_in user
+
+      redirect
+    else
+      flash.now[:danger] = t ".invalid"
+      render :new
+    end
+  end
+
+  def destroy
+    log_out if logged_in?
+
+    redirect_to root_path
+  end
+
+  private
+  def redirect
+    if current_user.admin?
+      redirect_to admin_root_path
+    else
+      redirect_to current_user
+    end
+  end
+end
